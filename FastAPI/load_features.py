b802280641f0141b2f2328cb7288269dc4f32d60
Sultan477 <156331129+Sultan477@users.noreply.github.com>
2024-07-26 13:34:15 +0300

    Create load_features.py

---
+++ FastAPI/load_features.py
@@ -0,0 +1,22 @@
+from database import Base, SessionLocal, engine
+import pandas as pd
+
+
+def batch_load_sql(query: str) -> pd.DataFrame:
+
+    CHUNKSIZE = 1000
+    conn = engine.connect().execution_options(stream_results=True)
+    chunks = []
+    for chunk_dataframe in pd.read_sql(query, conn,
+                                       chunksize=CHUNKSIZE):
+        chunks.append(chunk_dataframe)
+        print(chunk_dataframe)
+    conn.close()
+    data = pd.concat(chunks, ignore_index=True)
+
+    return data
+
+def load_features() -> pd.DataFrame:
+    query = "s_aktemirov_all_posts_new"
+    query2 = "s_aktemirov_all_users_new"
+    return batch_load_sql(query), batch_load_sql(query2)
